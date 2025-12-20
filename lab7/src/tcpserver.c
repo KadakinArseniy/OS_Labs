265265ffed41b413a1bcca9c4d3db856114f5ac9
KadakinArseniy <kadakin.senya@mail.ru>
2025-12-20 01:14:25 +0000

    lab7

--- lab7/src/tcpserver.c
+++ lab7/src/tcpserver.c
@@ -1,62 +1,124 @@
 #include <netinet/in.h>
+
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
 
+
 #include <sys/socket.h>
+
 #include <sys/types.h>
+
 #include <unistd.h>
 
+
 #define SERV_PORT 10050
+
 #define BUFSIZE 100
+
 #define SADDR struct sockaddr
 
-int main() {
+
+int main(int argc, char *argv[]) {
+
+  if (argc < 3) {
+
+    printf("Usage: %s <port> <buffer_size>\n", argv[0]);
+
+    exit(1);
+
+  }
+
+
   const size_t kSize = sizeof(struct sockaddr_in);
 
+
   int lfd, cfd;
+
   int nread;
-  char buf[BUFSIZE];
+
+  //char buf[BUFSIZE];
+
+  int buffer_size = atoi(argv[2]);
+
+  char buf[buffer_size];
+
   struct sockaddr_in servaddr;
+
   struct sockaddr_in cliaddr;
 
+
   if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+
     perror("socket");
+
     exit(1);
+
   }
+
 
   memset(&servaddr, 0, kSize);
+
   servaddr.sin_family = AF_INET;
+
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(SERV_PORT);
+
+  servaddr.sin_port = htons(atoi(argv[1]));
+
 
   if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
+
     perror("bind");
+
     exit(1);
+
   }
+
 
   if (listen(lfd, 5) < 0) {
+
     perror("listen");
+
     exit(1);
+
   }
+
 
   while (1) {
+
     unsigned int clilen = kSize;
 
+
     if ((cfd = accept(lfd, (SADDR *)&cliaddr, &clilen)) < 0) {
+
       perror("accept");
+
       exit(1);
+
     }
+
     printf("connection established\n");
 
-    while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
+
+    while ((nread = read(cfd, buf, buffer_size)) > 0) {
+
       write(1, &buf, nread);
+
     }
 
+
     if (nread == -1) {
+
       perror("read");
+
       exit(1);
+
     }
+
     close(cfd);
+
   }
+
 }
